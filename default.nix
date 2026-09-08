15ecbc5def1c4332d7d2b4f22be9344654030f9c
vvvince <vvvince@ofcompute.rs>
2026-09-08 20:57:59 +0200

    update libs

--- default.nix
+++ default.nix
@@ -4,7 +4,9 @@
 , autoPatchelfHook, wrapGAppsHook3, makeWrapper
 
 # Build inputs
-, zlib, xorg, alsa-lib, libbsd, libopus, openssl, libva, pango, cairo, libuuid, nspr
+, zlib, libx11, libxcb, libxrandr, libxcomposite, libxdamage, libxscrnsaver, libxcursor
+, libxfixes, libxi, libxtst, libxcb-image, libxcb-render-util, libxshmfence, libxinerama
+, alsa-lib, libbsd, libopus, openssl, libva, pango, cairo, libuuid, nspr
 , nss, cups, expat, atk, at-spi2-atk, gtk3, gdk-pixbuf, libsecret, systemd
 , pulseaudio, libGL, dbus, libnghttp2, libidn2, libpsl, libkrb5, openldap
 , rtmpdump, libinput, mesa, libpulseaudio, libvdpau, curl
@@ -51,19 +53,19 @@ in stdenv.mkDerivation rec {
   buildInputs = [
     stdenv.cc.cc.lib
 
-    xorg.libX11
-    xorg.libxcb
-    xorg.libXrandr
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXScrnSaver
-    xorg.libXcursor
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXtst
-    xorg.xcbutilimage
-    xorg.xcbutilrenderutil
-    xorg.libxshmfence
+    libx11
+    libxcb
+    libxrandr
+    libxcomposite
+    libxdamage
+    libxscrnsaver
+    libxcursor
+    libxfixes
+    libxi
+    libxtst
+    libxcb-image
+    libxcb-render-util
+    libxshmfence
 
     cairo
     pango
@@ -104,7 +106,7 @@ in stdenv.mkDerivation rec {
     libGL
     dbus
     libsecret
-    xorg.libXinerama
+    libxinerama
     libva
   ];
 
